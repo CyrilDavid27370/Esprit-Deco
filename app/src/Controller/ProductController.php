50b25e5193153f18ea3899b173e577020a028bf7
Cyril David <cdhinho27370@outlook.fr>
2026-04-29 16:12:33 +0200

    lot1-3 : Modification du ProductController

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('product/index.html.twig', [
+        $products = $productRepository->findAll();
 
+        return $this->render('product/index.html.twig', [
+            'products' => $products
         ]);
     }
 }
