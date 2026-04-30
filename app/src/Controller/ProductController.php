8f255adeabd4a0cf8f3a50c2b0fa6df524c85bf6
Cyril David <cdhinho27370@outlook.fr>
2026-04-30 16:19:28 +0200

    lot1-4 : ajout du queryBuilder dans ProductRepository et modif des pages index et show twig

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,16 +12,22 @@ final class ProductController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(ProductRepository $productRepository): Response
     {
-        $products = $productRepository->findAllWithCategoryAndImages();
+        $products = $productRepository->findAllWithCategoryAndPrincipalImage();
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
         ]);
     }
 
-    #[Route('/product/{id}', name: 'app_product_show',requirements: ['id' => '\d+'])]
-    public function show(Product $product):Response
+    #[Route('/product/{id}', name: 'app_product_show', requirements: ['id' => '\d+'])]
+    public function show(int $id, ProductRepository $productRepository): Response
     {
+        $product = $productRepository->findOneWithCategoryAndAllImages($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+
         return $this->render('product/show.html.twig', [
             'product' => $product,
         ]);
