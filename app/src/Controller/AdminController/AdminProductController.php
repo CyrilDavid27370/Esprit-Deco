ed554dc86d7b32dc0321ad73e3afda9097e6cd30
Cyril David <cdhinho27370@outlook.fr>
2026-05-05 10:17:20 +0200

    modification du dossier adminController

--- app/src/Controller/AdminController/AdminProductController.php
+++ app/src/Controller/AdminController/AdminProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\AdminController;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
