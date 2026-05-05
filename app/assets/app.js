fac9a1b2ea103e0689bfe6e7892260cf7afd53fc
Cyril David <cdhinho27370@outlook.fr>
2026-05-05 10:47:20 +0200

    création des asset mappers des autres fichiers css

--- app/assets/app.js
+++ app/assets/app.js
@@ -5,6 +5,10 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './js/bootstrap.js';
+import './js/app.general.js';
+import './js/header.js';
+import './js/nav.js';
+import './js/footer.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
