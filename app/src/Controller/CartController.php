5663bfc431dbdc0b2120880fdc05dfa7c47402a3
Cyril David <cdhinho27370@outlook.fr>
2026-05-07 17:12:19 +0200

    lot3-1-A : création du CartController et ajout du bouton panier dans nav.html.twig

---
+++ app/src/Controller/CartController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CartController extends AbstractController
+{
+    #[Route('/cart/add/{id}', name: 'app_cart_add', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function add(Product $product, SessionInterface $session, ProductRepository $productRepository): Response
+    {
+        $cart = $session->get('cart', []);
+
+        $productId = $product->getId();
+        $cart[$productId] = ($cart[$product->getId()] ?? 0) + 1;
+
+        $session->set('cart', $cart);
+
+        $this->addFlash('success', $product->getTitle() . 'a été ajouté au panier.');
+
+
+        return $this->redirectToRoute("app_product_list");
+    }
+}
