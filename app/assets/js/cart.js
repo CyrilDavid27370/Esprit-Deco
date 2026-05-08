63b5bc60b71146f17802411d05326fec8cc618bc
Cyril David <cdhinho27370@outlook.fr>
2026-05-08 18:27:19 +0200

    lot3-1-B : Création du dossier cart/js

---
+++ app/assets/js/cart.js
@@ -0,0 +1,38 @@
+document.addEventListener('turbo:load', () => {
+
+     // Bouton -
+    document.querySelectorAll('.btn-decrease').forEach(btn => {
+        btn.addEventListener('click', async () => {
+            const id = btn.dataset.id;
+            await fetch(`/cart/decrease/${id}`, { method: POST});
+            window.location.reload();
+        });
+    });
+
+    // Bouton +
+    document.querySelectorAll('.btn-increase').forEach(btn => {
+        btn.addEventListener('click', async () => {
+            const id = btn.dataset.id;
+            await fetch(`/cart/add/${id}`, { method: POST});
+            window.location.reload();
+        });
+    });
+
+    // Bouton supprimer un produit
+    document.querySelectorAll('.btn-remove').forEach(btn => {
+        btn.addEventListener('click', async () => {
+            const id = btn.dataset.id;
+            await fetch(`/cart/remove/${id}`, { method: POST});
+            window.location.reload();
+        });
+    });
+
+    // Bouton vider le panier
+    const btnClear = document.querySelector('.btn-clear');
+    if (btnClear) {
+        btnClear.addEventListener('click', async () => {
+            await fetch('/cart/clear', { method: 'POST' });
+            window.location.reload();
+        });
+    }
+});
