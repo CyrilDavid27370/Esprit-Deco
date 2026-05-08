4b0f2618f912c65e596136a52d50710e4ebd2087
Cyril David <cdhinho27370@outlook.fr>
2026-05-08 18:08:57 +0200

    lot3-1-B : creation des routes decrease,remove et clear dans le CartController

--- app/src/Controller/CartController.php
+++ app/src/Controller/CartController.php
@@ -8,22 +8,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
-
 final class CartController extends AbstractController
 {
-    #[Route('/cart/add/{id}', name: 'app_cart_add', requirements: ['id' => '\d+'], methods: ['POST'])]
-    public function add(Product $product, SessionInterface $session, ProductRepository $productRepository): Response
+    public function __construct(
+        private SessionInterface $session
+    )
     {
-        $cart = $session->get('cart', []);
+    }
+    #[Route('/cart/add/{id}', name: 'app_cart_add', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function add(Product $product,ProductRepository $productRepository): Response
+    {
+        $cart = $this->session->get('cart', []);
 
         $productId = $product->getId();
         $cart[$productId] = ($cart[$product->getId()] ?? 0) + 1;
 
-        $session->set('cart', $cart);
+        $this->session->set('cart', $cart);
 
         $this->addFlash('success', $product->getTitle() . ' ' . ' a été ajouté au panier');
 
 
         return $this->redirectToRoute("app_home");
     }
+
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function decrease(Product $product): Response
+    {
+        $cart = $this->session->get('cart', []);
+        $id = $product->getId();
+
+        if (isset($cart[$id])) {
+            if ($cart[$id] > 1) {
+                $cart[$id]--;
+            } else {
+                unset($cart[$id]);
+            }
+        }
+
+        $this->session->set('cart', $cart);
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'app_cart_remove', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function remove(Product $product): Response
+    {
+        $cart = $this->session->get('cart', []);
+        unset($cart[$product->getId()]);
+        $this->session->set('cart', $cart);
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/cart/clear', name: 'app_cart_clear', methods: ['POST'])]
+    public function clear(): Response
+    {
+        $this->session->remove('cart');
+        return $this->redirectToRoute('app_home');
+    }
+
 }
