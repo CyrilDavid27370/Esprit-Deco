27a9050f3c1a8567fbed7a6a369da5dde7a35168
Cyril David <cdhinho27370@outlook.fr>
2026-05-09 13:55:04 +0200

    lot3-1-C : modification de la méthode add dans CartController

--- app/src/Controller/CartController.php
+++ app/src/Controller/CartController.php
@@ -9,28 +9,42 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\CartHandler;
+use Symfony\Bundle\SecurityBundle\Security;
 
 final class CartController extends AbstractController
 {
     public function __construct(
-        private RequestStack $requestStack
+        private RequestStack $requestStack,
+        private CartHandler $cartHandler,
+        private Security $security,
     ) {}
 
     #[Route('/cart/add/{id}', name: 'app_cart_add', requirements: ['id' => '\d+'], methods: ['POST'])]
-    public function add(Product $product, ProductRepository $productRepository, Request $request): Response
-    {
-        $session = $this->requestStack->getSession();
-        $cart = $session->get('cart', []);
-        $productId = $product->getId();
-        $cart[$productId] = ($cart[$productId] ?? 0) + 1;
-        $session->set('cart', $cart);
+    public function add(Product $product, Request $request): Response
+    {   
+        $user = $this->security->getUser();
 
+        if ($user) {
+            $this->cartHandler->addToDb($product);
+            $totalQuantity = $this->cartHandler->getTotalQuantity();
+            $quantity = $this->cartHandler->getProductQuantity($product);
+        } else {
+            $session = $this->requestStack->getSession();
+            $cart = $session->get('cart', []);
+            $productId = $product->getId();
+            $cart[$productId] = ($cart[$productId] ?? 0) + 1;
+            $session->set('cart', $cart);
+            $totalQuantity = array_sum($cart);
+            $quantity = $cart[$productId];
+        }
+        
         // Si requête AJAX → retourne JSON
         if ($request->isXmlHttpRequest()) {
             return $this->json([
                 'success' => true,
-                'quantity' => $cart[$productId],
-                'totalQuantity' => array_sum($cart),
+                'quantity' => $quantity,
+                'totalQuantity' => $totalQuantity,
             ]);
         }
 
